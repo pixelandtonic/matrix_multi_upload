333ab5e7d9b2210a11294c958f88432881716023
Brandon Kelly <me@brandon-kelly.com>
2010-08-16 12:42:05 -0700

    description

--- system/third_party/matrix_multi_upload/acc.matrix_multi_upload.php
+++ system/third_party/matrix_multi_upload/acc.matrix_multi_upload.php
@@ -14,7 +14,7 @@ class Matrix_multi_upload_acc {
 	var $name        = 'Matrix Multi-Upload';
 	var $id          = 'matrix_multi_upload';
 	var $version     = '0.9';
-	var $description = 'My accessory has a lovely description.';
+	var $description = 'Upload multiple files to Matrix at once, powered by <a href="http://www.plupload.com/">Plupload</a>';
 	var $sections    = array();
 
 	/**
--- system/third_party/matrix_multi_upload/language/english/lang.matrix_multi_upload.php
+++ system/third_party/matrix_multi_upload/language/english/lang.matrix_multi_upload.php
@@ -8,7 +8,7 @@ $lang = array(
 // -------------------------------------------
 
 'matrix_multi_upload_module_name'        => 'Matrix Multi-Upload',
-'matrix_multi_upload_module_description' => 'Upload multiple files to Matrix at once',
+'matrix_multi_upload_module_description' => 'Upload multiple files to Matrix at once, powered by <a href="http://www.plupload.com/">Plupload</a>',
 
 // -------------------------------------------
 //  Errors
